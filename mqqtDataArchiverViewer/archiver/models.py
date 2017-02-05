950e964e78908e5aec3ddcb447cbb0d42cbe553f
stevemolloy <sdmolloy@gmail.com>
2017-02-05 22:11:04 +0100

    Added new models for the water system

--- mqqtDataArchiverViewer/archiver/models.py
+++ mqqtDataArchiverViewer/archiver/models.py
@@ -16,3 +16,28 @@ class cpm(models.Model):
 
     def __str__(self):
         return "{}: {}".format(self.timestamp, self.measured_val)
+
+class KlystronBodyWaterSystem(models.Model):
+    body_flow = models.FloatField()
+    body_temp = models.FloatField(null=True)
+    timestamp = models.DateTimeField()
+
+class ModOilTankWaterSystem(models.Model):
+    tank_flow = models.FloatField()
+    tank_temp = models.FloatField(null=True)
+    timestamp = models.DateTimeField()
+
+class KlystronCollectorWaterSystem(models.Model):
+    collector_flow = models.FloatField()
+    collector_temp = models.FloatField(null=True)
+    timestamp = models.DateTimeField()
+
+class KlystronSolenoidWaterSystem(models.Model):
+    solenoid_flow = models.FloatField()
+    solenoid_temp = models.FloatField(null=True)
+    timestamp = models.DateTimeField()
+
+class InputWaterSystem(models.Model):
+    input_flow = models.FloatField(null=True)
+    input_temp = models.FloatField()
+    timestamp = models.DateTimeField()
