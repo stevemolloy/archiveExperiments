716f8dd2694cb0380f53fd312b927aefe578a6e5
stevemolloy <sdmolloy@gmail.com>
2017-02-05 16:03:26 +0100

    Added a registry for the web-viewer

--- mqqtDataArchiverViewer/archiver/models.py
+++ mqqtDataArchiverViewer/archiver/models.py
@@ -2,6 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class registry(models.Model):
+    signal = models.CharField(max_length=150)
+    archival_active = models.BooleanField()
+    first_registered = models.DateTimeField()
+    last_altered = models.DateTimeField()
+
 class cpm(models.Model):
     measured_val = models.IntegerField()
     timestamp = models.DateTimeField()
