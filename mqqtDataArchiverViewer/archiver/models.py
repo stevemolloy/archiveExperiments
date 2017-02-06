a78f3ccdfeadd63f92f10fc4e994bf1e3e54401c
stevemolloy <sdmolloy@gmail.com>
2017-02-06 09:29:07 +0100

    Added __str__ to the registry class

--- mqqtDataArchiverViewer/archiver/models.py
+++ mqqtDataArchiverViewer/archiver/models.py
@@ -3,11 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
+@python_2_unicode_compatible
 class registry(models.Model):
     signal = models.CharField(max_length=150)
     archival_active = models.BooleanField()
     first_registered = models.DateTimeField()
     last_altered = models.DateTimeField()
+
+    def __str__(self):
+        return "{}: {}".format(self.signal, self.archival_active)
 
 @python_2_unicode_compatible
 class cpm(models.Model):
