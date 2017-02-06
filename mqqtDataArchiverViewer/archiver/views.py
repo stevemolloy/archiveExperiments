f1e827bd87e0061513e264bba1599d1ad63d80cd
stevemolloy <sdmolloy@gmail.com>
2017-02-06 13:11:33 +0100

    Added some views and templates

--- mqqtDataArchiverViewer/archiver/views.py
+++ mqqtDataArchiverViewer/archiver/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import registry
 
-# Create your views here.
+def index(request):
+    latest_signal_list = registry.objects.order_by('-first_registered')[:5]
+    context = {'latest_signal_list': latest_signal_list,}
+    return render(request, 'archiver/index.html', context)
+
+def signalDetail(request, signal_id):
+    return HttpResponse("Some details about signal #{}".format(signal_id))
