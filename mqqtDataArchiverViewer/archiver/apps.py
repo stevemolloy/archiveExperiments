2abe86b435f373020e65998bb69fc5060cb38e61
stevemolloy <sdmolloy@gmail.com>
2017-02-09 11:22:17 +0100

    Removed unnecessary imports

--- mqqtDataArchiverViewer/archiver/apps.py
+++ mqqtDataArchiverViewer/archiver/apps.py
@@ -1,13 +1,6 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
-from django.utils import timezone
-from django.db.models.signals import post_save
-from threading import Thread
-import paho.mqtt.client as mqtt
-import json
-import time
-import archiver
 from .mqtt import startMQTT
 
 class ArchiverConfig(AppConfig):
