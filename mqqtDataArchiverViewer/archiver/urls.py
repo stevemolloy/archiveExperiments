eeaa33e6a2a4fd7d7daaa779c5d74fc294129827
stevemolloy <sdmolloy@gmail.com>
2017-02-09 16:04:49 +0100

    Fixed bugs in urls.py

--- mqqtDataArchiverViewer/archiver/urls.py
+++ mqqtDataArchiverViewer/archiver/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<signal_id>[0-9]+)/$', views.signalDetail, name='detail'),
     url(r'^updateRegistry$', views.updateRegistry, name='updateRegistry'),
-    url(r'^addNewSignal$', views.updateRegistry, name='addNewSignal'),
+    url(r'^addNewSignal$', views.addNewSignal, name='addNewSignal'),
 ]
